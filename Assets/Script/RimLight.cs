ce021014840df3e03aa95e73da19361d0e46bdcb
shiweiwei <shiabo1121@126.com>
2018-08-30 22:29:00 +0800

    Add Unity Project
    增加Unity工程，Unity版本4.7.2

---
+++ Assets/Script/RimLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RimLight : MonoBehaviour {
+
+    Material mat;
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+        mat.SetFloat("_CurrentTime",Time.timeSinceLevelLoad);
+        StartCoroutine(SetTime());
+	}
+
+    IEnumerator SetTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            mat.SetFloat("_CurrentTime", Time.timeSinceLevelLoad);
+        }
+    }
+}
